4b4bf761374921c1f395d4181f033cc56c73a88c
wwwchendev <wwwchendev@gmail.com>
2024-02-16 14:47:04 +0800

    summary Form - code,test and pass + 使用userEvent取代fireEvent

--- sundae-starter/src/pages/summary/SummaryForm.jsx
+++ sundae-starter/src/pages/summary/SummaryForm.jsx
@@ -1,6 +1,31 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+
 export default function SummaryForm() {
+  const [tcChecked, setTcChecked] = useState(false);
+
+  const checkboxLabel = (
+    <span>
+      我已閱讀並同意
+      <span style={{ color: "#05eeff" }}> 訂購規定</span>
+    </span>
+  );
 
   return (
-    <div>SummaryForm</div>
+    <Form>
+      <Form.Group controlId="terms-and-conditions">
+        <Form.Check
+          type="checkbox"
+          checked={tcChecked}
+          onChange={(e) => setTcChecked(e.target.checked)}
+          label={checkboxLabel}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit" disabled={!tcChecked}>
+        確認訂單
+      </Button>
+    </Form>
   );
 }
--- sundae-starter/src/pages/summary/tests/SummaryForm.test.jsx
+++ sundae-starter/src/pages/summary/tests/SummaryForm.test.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable vitest/no-commented-out-tests */
 import { render, screen, fireEvent } from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
 
 test("測試初始狀態", () => {
   render(<SummaryForm />);
@@ -12,14 +13,15 @@ test("測試初始狀態", () => {
   expect(confirmButton).toBeDisabled();
 });
 
-test("Checkbox在第一次單擊時啟用按鈕，再次單擊時停用", () => {
+test("Checkbox在第一次單擊時啟用按鈕，再次單擊時停用", async () => {
+  const user = userEvent.setup();
   render(<SummaryForm />);
   const checkbox = screen.getByRole("checkbox", { name: /訂購規定/i });
   const confirmButton = screen.getByRole("button", { name: /確認訂單/i });
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmButton).toBeEnabled();
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmButton).toBeDisabled();
 });
