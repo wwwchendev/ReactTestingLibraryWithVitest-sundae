bc5f104a464cb04a650a2c15ae499e4eab2667b2
wwwchendev <wwwchendev@gmail.com>
2024-02-16 23:34:49 +0800

    顯示聖代配料圖片(串api) - code,test and pass

--- sundae-starter/src/pages/entry/ToppingOption.jsx
+++ sundae-starter/src/pages/entry/ToppingOption.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import Col from "react-bootstrap/Col";
 
-const ToppingOption = () => {
+export default function ToppingOption({ name, imagePath }) {
   return (
-    <div>ToppingOption</div>
-  )
+    <Col xs={6} sm={4} md={3} lg={2} style={{ textAlign: "center" }}>
+      <img
+        style={{ width: "75%" }}
+        src={`http://localhost:3030/${imagePath}`}
+        alt={`${name} topping`}
+      />
+    </Col>
+  );
 }
-
-export default ToppingOption
